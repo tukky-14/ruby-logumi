6202ae1be16f1d90ca5ff55f29e74de1106bab5e
Eiji Otsuki <bigoonage0104@gmail.com>
2020-05-29 17:24:36 +0900

    Create gummy delete modal

--- app/assets/javascripts/gummies.js
+++ app/assets/javascripts/gummies.js
@@ -6,6 +6,17 @@ $(function() {
   }).mouseout(function() {
     $(this).removeClass("gummi-hover");
   })
+
+  // グミ削除時モーダル
+  // ------------------------------------
+  $('#modal').click(function() {
+    $('.glay_layer').slideDown();
+    $('.delete_modal').slideDown();
+  })
+  $('#modal_back').click(function() {
+    $('.glay_layer').slideUp();
+    $('.delete_modal').slideUp();
+  })
 });
 
 
