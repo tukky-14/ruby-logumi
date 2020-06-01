16922e6c67a26562db959800595972d5f7a82742
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-01 17:49:04 +0900

    Create favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_090851) do
+ActiveRecord::Schema.define(version: 2020_06_01_074805) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_05_27_090851) do
     t.datetime "updated_at", null: false
     t.index ["gummy_id"], name: "index_comments_on_gummy_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "gummy_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "gummies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
---
+++ spec/factories/favorites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :favorite do
+    user_id { 1 }
+    gummy_id { 1 }
+  end
+end
