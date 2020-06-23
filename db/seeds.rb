518bde556af64490930597901ad0ca73715b311f
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-23 16:12:29 +0900

    Create user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,36 @@
-# User.create!(
-#   name:
-#   email:
-#   password:
-# )
+# テストユーザ
+# ------------------------------------
+User.create!(
+  name: 'test',
+  email:'test@sample',
+  password:'000000',
+  like:'つぶグミ、コグミ',
+  introduction: 'たくさん入っているグミが好きです。',
+  image:File.open("./public/images/users/test.jpg"),
+)
+User.create!(
+  name:'testman',
+  email:'testman@sample',
+  password:'000000',
+  like:'カンデミーナ、タフグミ',
+  introduction: '硬いグミが大好きです！',
+  image:File.open("./public/images/users/testman.jpg"),
+)
+User.create!(
+  name:'gummiko',
+  email:'gummiko@sample',
+  password:'000000',
+  like:'フィットチーネグミ',
+  introduction: 'グミ美味しい。',
+  image:File.open("./public/images/users/gummiko.jpg"),
+)
 
-# Gummy.create!(
-#   name:
-#   content:
-#   image:
-#   user_id:
-#   maker_id:
-# )
+# テストグミ
+# ------------------------------------
+Gummy.create!(
+  name: '果汁グミ',
+  content: 'やっぱり果汁グミです！！',
+  image:File.open("./public/images/gummies/kaju.png"),
+  user_id: 1,
+  maker_id: 3,
+)
