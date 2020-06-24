589c1ee86b318f7f43c20502352c0aeb1e8df30f
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-24 17:02:21 +0900

    Add image preview function

---
+++ app/assets/javascripts/new_gummi.js
@@ -0,0 +1,23 @@
+// 投稿画面の画像プレビュー表示
+
+$(function(){
+  $('#form__list__image').change(function(){
+    const file = $('input[type="file"]').prop('files')[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = function() {
+      const src = fileReader.result
+      const html= `<div>
+                  <img src="${src}" class="form__image__preview">
+                  <p class="form__image__delete">削除</p>
+                  </div>`
+      $('.form__image').append(html);
+    }
+    fileReader.readAsDataURL(file);
+  });
+
+  $('.form__image').on("click", '.form__image__delete', function(){
+    const target_image = $(this).parent()
+    target_image.remove();
+    $('#form__list__image').val("");
+  })
+});
