55674f088050e2afcccc442bf77e9d624fa12c11
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-28 19:33:44 +0900

    Configure responsive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @user_id = user.id
-    @name = user.name
-    @like = user.like
-    @image = user.image
-    @introduction = user.introduction
-    @gummies = user.gummies.order("created_at DESC")
+    @user = User.find(params[:id])
+    @gummies = @user.gummies.order("created_at DESC")
   end
 end
